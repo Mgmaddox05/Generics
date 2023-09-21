46c3f4fba51cba093acfee24f157e33b01c95ad4
Mgmaddox05 <125920278+Mgmaddox05@users.noreply.github.com>
2023-09-21 15:31:29 -0500

    Update

---
+++ Generics/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Stack<Integer> int_stack1 = new Stack<>();
+        int_stack1.push(3);
+        System.out.println(int_stack1.pop());
+    }
+}
